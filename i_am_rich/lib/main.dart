049d69e40f051cbe6347c54c675e9c92e71974f6
Bence Fulop <fbence90@gmail.com>
2021-03-10 13:35:10 +1100

    add picture to body

--- i_am_rich/lib/main.dart
+++ i_am_rich/lib/main.dart
@@ -1,3 +1,20 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(home: Center(child: Text('Hello World'))));
+void main() {
+  runApp(
+    MaterialApp(
+      // Removes 'debug' banner from emulator
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          title: Text("I Am Rich!"),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        body: Image(
+          image: NetworkImage('https://source.unsplash.com/random'),
+        ),
+      ),
+    ),
+  );
+}
