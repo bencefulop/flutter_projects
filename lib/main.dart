9d5d1f320825a5f3ce47f4a26be8f7aed83b8954
Bence Fulop <fbence90@gmail.com>
2021-03-13 11:24:56 +1100

    re-adding files to sub module

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,47 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: Column(
+            children: [
+              CircleAvatar(
+                radius: 60,
+                backgroundImage: AssetImage('images/avatar.jpg'),
+              ),
+              Text(
+                'Bence Fulop',
+                style: TextStyle(
+                  fontSize: 40,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'Pacifico',
+                ),
+              ),
+              SizedBox(height: 10),
+              Text(
+                'FLUTTER DEVELOPER',
+                style: TextStyle(
+                  fontSize: 20,
+                  color: Colors.grey[300],
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'SourceSansPro',
+                  letterSpacing: 3,
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
