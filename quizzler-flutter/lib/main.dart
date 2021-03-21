568a3e1cd09315f33d1e1c95c4cc94dba60572eb
Bence Fulop <fbence90@gmail.com>
2021-03-21 12:59:46 +1100

    Added questions and answer Icons - tick or cross appears on bottom of the screen when true or false is onPressed - questions cycle on button presses

--- quizzler-flutter/lib/main.dart
+++ quizzler-flutter/lib/main.dart
@@ -25,6 +25,19 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
+  List<Icon> scoreKeeper = [];
+  List<String> questions = [
+    'You can lead a cow down stairs but not up stairs.',
+    'Approximately one quarter of human bones are in the feet.',
+    'A slug\'s blood is green.'
+  ];
+
+  int questionNumber = 0;
+
+  void nextQuestion() {
+    (questionNumber < 2) ? questionNumber += 1 : questionNumber = 0;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -37,7 +50,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'This is where the question text will go.',
+                questions[questionNumber],
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -61,7 +74,17 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
-                //The user picked true.
+                setState(
+                  () {
+                    scoreKeeper.add(
+                      Icon(
+                        Icons.check,
+                        color: Colors.green,
+                      ),
+                    );
+                    nextQuestion();
+                  },
+                );
               },
             ),
           ),
@@ -79,12 +102,24 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
-                //The user picked false.
+                setState(
+                  () {
+                    scoreKeeper.add(
+                      Icon(
+                        Icons.close,
+                        color: Colors.red,
+                      ),
+                    );
+                    nextQuestion();
+                  },
+                );
               },
             ),
           ),
         ),
-        //TODO: Add a Row here as your score keeper
+        Row(
+          children: scoreKeeper,
+        )
       ],
     );
   }
