98a4c77ab4807913ccaec8a59dc9b1a7f1e9d3e3
Bence Fulop <fbence90@gmail.com>
2021-04-17 12:20:37 +1000

    change route strings to class variable

--- flash-chat-flutter-master/lib/main.dart
+++ flash-chat-flutter-master/lib/main.dart
@@ -20,12 +20,12 @@ class FlashChat extends StatelessWidget {
           bodyText2: TextStyle(color: Colors.black54),
         ),
       ),
-      initialRoute: '/',
+      initialRoute: WelcomeScreen.id,
       routes: {
-        '/': (context) => WelcomeScreen(),
-        '/chat': (context) => ChatScreen(),
-        '/login': (context) => LoginScreen(),
-        '/registration': (context) => RegistrationScreen()
+        WelcomeScreen.id: (context) => WelcomeScreen(),
+        ChatScreen.id: (context) => ChatScreen(),
+        LoginScreen.id: (context) => LoginScreen(),
+        RegistrationScreen.id: (context) => RegistrationScreen()
       },
     );
   }
