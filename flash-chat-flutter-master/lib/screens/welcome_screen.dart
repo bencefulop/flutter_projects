e0cbc9e0705da941144aca21a7d421259998065d
Bence Fulop <fbence90@gmail.com>
2021-04-17 15:26:49 +1000

    added animation to title and background color transition

--- flash-chat-flutter-master/lib/screens/welcome_screen.dart
+++ flash-chat-flutter-master/lib/screens/welcome_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:animated_text_kit/animated_text_kit.dart';
 
 import 'login_screen.dart';
 import 'registration_screen.dart';
@@ -23,20 +24,12 @@ class _WelcomeScreenState extends State<WelcomeScreen>
       vsync: this,
       upperBound: 1,
     );
-    animation = CurvedAnimation(parent: controller, curve: Curves.decelerate);
+    animation = ColorTween(begin: Colors.blueGrey, end: Colors.white)
+        .animate(controller);
     controller.forward();
-    // controller.reverse(from: 1.0);
 
-    animation.addStatusListener((status) {
-      if (status == AnimationStatus.completed) {
-        controller.reverse(from: 1.0);
-      } else if (status == AnimationStatus.dismissed) {
-        controller.forward();
-      }
-    });
     controller.addListener(() {
       setState(() {});
-      print(animation.value);
     });
   }
 
@@ -49,7 +42,7 @@ class _WelcomeScreenState extends State<WelcomeScreen>
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
+      backgroundColor: animation.value,
       body: Padding(
         padding: EdgeInsets.symmetric(horizontal: 24.0),
         child: Column(
@@ -62,14 +55,25 @@ class _WelcomeScreenState extends State<WelcomeScreen>
                   tag: 'logo',
                   child: Container(
                     child: Image.asset('images/logo.png'),
-                    height: animation.value * 100,
+                    height: 60,
                   ),
                 ),
-                Text(
-                  'Flash Chat',
-                  style: TextStyle(
-                    fontSize: 45.0,
-                    fontWeight: FontWeight.w900,
+                SizedBox(
+                  child: DefaultTextStyle(
+                    style: TextStyle(
+                      fontSize: 45.0,
+                      fontWeight: FontWeight.w900,
+                      color: Colors.black,
+                    ),
+                    child: AnimatedTextKit(
+                      animatedTexts: [
+                        TypewriterAnimatedText(
+                          'Flash Chat',
+                          cursor: '|',
+                          speed: Duration(milliseconds: 80),
+                        ),
+                      ],
+                    ),
                   ),
                 ),
               ],
